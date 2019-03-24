95af9e080bba803be7329fcdc4870308330585e7
WHMHammer <35433952+WHMHammer@users.noreply.github.com>
2019-03-24 00:35:42 -0400

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,15 +1,15 @@
-drop database auth if exits;
 create database auth character set utf8mb4;
+
 use auth;
 
 create table users(
-    id int primary key,
-    username varchar(32) unique,
-    salt char(32),
+    id int auto_increment primary key,
+    username varchar(64) unique,
+    salt char(16),
     password_hash char(128),
     email varchar(64) unique,
     catagory varchar(32),
     status varchar(32) default 'unverified',
     last_login_time int(10) default 0,
-    challenge char(32)
+    challenge char(16)
 );
