8aad157c7e9ab23646f1848dd21bc013f2a2c4a1
WHMHammer <35433952+WHMHammer@users.noreply.github.com>
2019-04-03 12:11:44 -0400

    Add files via upload

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,17 @@
+import flask
 
+def create_app():
+    app=flask.Flask(__name__)
+    app.secret_key="secret_key"
+    
+    
+    @app.route("/",methods=("GET",))
+    def root():
+        return flask.redirect("/auth/login")
+    
+    # register blueprints    
+    from . import auth
+    app.register_blueprint(auth.bp)
+    
+    
+    return app
